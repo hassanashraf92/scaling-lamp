5ec15de4f3e7f5370d824cd3b033e39e074a1f74
Hassan Ashraf <hassan.ashraf@vodafone.com>
2022-09-16 17:34:31 +0200

    feat: project init

---
+++ WHWeatherApp/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  WHWeatherApp
+//
+//  Created by Hassan Ashraf on 16/09/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
